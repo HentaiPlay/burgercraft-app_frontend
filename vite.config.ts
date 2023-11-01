dde3fbc217f35ac63059e0d18ab55be138d49cfa
HentaiPlay <david.tonoyan16@mail.ru>
2023-11-01 07:38:48 +0300

    Добавлен плагин eslinter, добавлен alias к корневой директории

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
-export default defineConfig({  
-  plugins: [vue()],
+export default defineConfig({
+  plugins: [
+    vue(),
+    eslintPlugin(),
+    checker({
+      vueTsc: true
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      find: '@vue/runtime-core',
+      replacement: '@vue/runtime-core/dist/runtime-core.esm-bundler.js'
+    },
+    extensions: ['.js', '.ts', '.vue']
+  },
+  server: {
+    port: 5000,
+    watch: {
+      usePolling: true
     }
   }
 })
