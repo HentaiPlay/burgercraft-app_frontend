99b7dd21f9e76231fbaa414c2c9f61f49b205ed7
HentaiPlay <david.tonoyan16@mail.ru>
2023-11-02 02:31:34 +0300

    Добавлена pinia

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './providers'
+import { store } from './providers'
 
-export const app = createApp(App).use(router)
+export const app = createApp(App).use(router).use(store)
