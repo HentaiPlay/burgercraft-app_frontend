88d495e2475ddd70c7fea306ead725e543c2dd5e
HentaiPlay <david.tonoyan16@mail.ru>
2023-11-09 02:17:15 +0300

    Добавлен element plus

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './providers'
 import { store } from './providers'
+import ElementPlus from 'element-plus'
 
-export const app = createApp(App).use(router).use(store)
+export const app = createApp(App).use(router).use(store).use(ElementPlus)
