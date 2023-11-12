0093a1cd5cbe23ba26ec22c8a66796966824c7f4
HentaiPlay <david.tonoyan16@mail.ru>
2023-11-12 23:45:59 +0300

    Удален прямой импорт Element plus

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './providers'
 import { store } from './providers'
-import ElementPlus from 'element-plus'
 
-export const app = createApp(App).use(router).use(store).use(ElementPlus)
+export const app = createApp(App).use(router).use(store)
