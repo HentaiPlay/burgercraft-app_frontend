c7a25d3efacc1dd81c87ff7aa7909e1f61aea5cb
HentaiPlay <david.tonoyan16@mail.ru>
2023-11-13 00:00:19 +0300

    Добавлено подключение стилей

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './providers'
 import { store } from './providers'
+import '@assets/styles/index.scss'
 
 export const app = createApp(App).use(router).use(store)
