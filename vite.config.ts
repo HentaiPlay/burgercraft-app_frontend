a5954dc74f5380f4dc99322c34c6972b6402148c
HentaiPlay <david.tonoyan16@mail.ru>
2023-11-12 23:59:33 +0300

    Добавлен алиас для ресурсов, добавлен плагин импорта компонентов, добавлен импорт стилей в приложений с глобальным определением миксинов и цветов и переопределенеим стилей element plus

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,51 @@ import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import eslintPlugin from 'vite-plugin-eslint'
 import checker from 'vite-plugin-checker'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    eslintPlugin(),
-    checker({
-      vueTsc: true
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      find: '@vue/runtime-core',
-      replacement: '@vue/runtime-core/dist/runtime-core.esm-bundler.js'
-    },
-    extensions: ['.js', '.ts', '.vue']
-  },
   server: {
     port: 5000,
     watch: {
       usePolling: true
     }
-  }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/shared'),
+      find: '@vue/runtime-core',
+      replacement: '@vue/runtime-core/dist/runtime-core.esm-bundler.js'
+    },
+    extensions: ['.js', '.ts', '.vue']
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@assets/styles/element/index.scss" as *;
+          @use "@assets/styles/_mixins.scss" as mixins;
+          @use "@assets/styles/_colors.scss" as colors;
+        `
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    eslintPlugin(),
+    checker({
+      vueTsc: true
+    }),
+    Components({
+      extensions: ['vue', 'md'],
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass'
+        })
+      ],
+      dts: 'src/components.d.ts'
+    })
+  ]
 })
