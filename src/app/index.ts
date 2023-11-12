504537b992b766b1ef95a82da1f7b5d02dc728c9
HentaiPlay <david.tonoyan16@mail.ru>
2023-11-13 01:21:26 +0300

    Добавлена установка плагина кастомных компонентов  ui kit

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,5 +3,6 @@ import App from './App.vue'
 import { router } from './providers'
 import { store } from './providers'
 import '@assets/styles/index.scss'
+import uiKit from '@/shared/ui-kit'
 
-export const app = createApp(App).use(router).use(store)
+export const app = createApp(App).use(router).use(store).use(uiKit)
