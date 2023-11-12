3120b60e1407d4937cce65523434a0e75960a136
HentaiPlay <david.tonoyan16@mail.ru>
2023-11-13 01:22:54 +0300

    Добавлена домашняя страница с тестовой странице для раработки ui kit-a

---
+++ src/pages/test/TestPage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { useDark, useToggle } from '@vueuse/core'
+  const isDark = useDark()
+  const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <div class="d-flex flex-column">
+    <el-button @click="toggleDark()">Сменить тему</el-button>
+
+    <my-test-component />
+
+    <a href="https://google.com">какая та ссылка</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  div {
+    background-color: colors.$bg-color;
+    color: var(--el-color-primary) !important;
+    @include mixins.color-scheme(red, green);
+  }
+  a {
+    color: colors.$primary;
+  }
+</style>
