72e72c69478d746b4363f197f687126f592b7f8b
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-16 23:23:41 +0300

    Добавлен дефолтный layout

---
+++ src/widgets/layouts/ui/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="default-layout">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .default-layout {
+    height: 100vh;
+    background: linear-gradient(colors.$bg-color, 85%, colors.$bg-color-page);
+  }
+</style>
