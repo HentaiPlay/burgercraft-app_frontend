5c0eb2487a5a4c6b13644479d4b6065474b8de58
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-16 23:03:53 +0300

    Обновлена карта компонентов

--- src/shared/components.d.ts
+++ src/shared/components.d.ts
@@ -7,16 +7,16 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ElAside: typeof import('element-plus/es')['ElAside']
-    ElButton: typeof import('element-plus/es')['ElButton']
-    ElContainer: typeof import('element-plus/es')['ElContainer']
-    ElFooter: typeof import('element-plus/es')['ElFooter']
-    ElHeader: typeof import('element-plus/es')['ElHeader']
-    ElInput: typeof import('element-plus/es')['ElInput']
-    ElMain: typeof import('element-plus/es')['ElMain']
-    ElOption: typeof import('element-plus/es')['ElOption']
-    ElSelect: typeof import('element-plus/es')['ElSelect']
-    RouterLink: typeof import('vue-router')['RouterLink']
-    RouterView: typeof import('vue-router')['RouterView']
+    ElAside: (typeof import('element-plus/es'))['ElAside']
+    ElButton: (typeof import('element-plus/es'))['ElButton']
+    ElContainer: (typeof import('element-plus/es'))['ElContainer']
+    ElFooter: (typeof import('element-plus/es'))['ElFooter']
+    ElHeader: (typeof import('element-plus/es'))['ElHeader']
+    ElInput: (typeof import('element-plus/es'))['ElInput']
+    ElMain: (typeof import('element-plus/es'))['ElMain']
+    ElOption: (typeof import('element-plus/es'))['ElOption']
+    ElSelect: (typeof import('element-plus/es'))['ElSelect']
+    RouterLink: (typeof import('vue-router'))['RouterLink']
+    RouterView: (typeof import('vue-router'))['RouterView']
   }
 }
