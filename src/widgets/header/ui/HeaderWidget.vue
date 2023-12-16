a5a958f46aec463f35780cc9e4482fe077b6a1fb
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-16 23:29:00 +0300

    Добавлен виджет header

---
+++ src/widgets/header/ui/HeaderWidget.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { useRouter } from 'vue-router'
+  const route = useRouter()
+  const toHome = () => route.push('/')
+</script>
+
+<template>
+  <el-header class="header">
+    <MyLogo
+      size="min"
+      show-title
+      @click="toHome"
+    />
+  </el-header>
+</template>
+
+<style lang="scss" scoped>
+  .header {
+    display: flex;
+    align-items: center;
+    height: variables.$header-height;
+    background-color: colors.$bg-color-page;
+    box-shadow: 2px 2px 10px colors.$bg-color-overlay;
+    z-index: 9999 !important;
+  }
+</style>
