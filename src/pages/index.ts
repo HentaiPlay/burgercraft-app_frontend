bce4103360c2d4e59e219a4a30a6e09ffed68cf0
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-16 23:15:31 +0300

    Добавлена meta для роутов, с шаблонов страниц приложений

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,8 +2,16 @@ import type { RouteRecordRaw } from 'vue-router'
 
 export const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
+    path: '/test',
     name: 'test',
-    component: () => import('./test/TestPage.vue')
+    component: () => import('./test/TestPage.vue'),
+    meta: {
+      layout: 'app'
+    }
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    component: () => import('./auth/AuthPage.vue')
   }
 ]
