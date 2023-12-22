791067fd9c50c10c8a7a5f1c779ff51c3a60c25b
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-22 17:47:00 +0300

    Изменен фон шаблона на картинку, фикс верстки

--- src/widgets/layouts/ui/DefaultLayout.vue
+++ src/widgets/layouts/ui/DefaultLayout.vue
@@ -2,13 +2,32 @@
 
 <template>
   <div class="default-layout">
-    <slot></slot>
+    <el-container>
+      <el-main class="main__content">
+        <slot></slot>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <style lang="scss" scoped>
   .default-layout {
     height: 100vh;
-    background: linear-gradient(colors.$bg-color, 85%, colors.$bg-color-page);
+    background-size: cover;
+    background-image: url('@assets/assets/images/default_layout_background.jpeg');
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: rgba(142, 115, 199, 0.25);
+      z-index: 2;
+    }
+    .main__content {
+      position: relative;
+      z-index: 3;
+    }
   }
 </style>
