ffe201271028b1a19a5ef87f0d3b18f689c2b36f
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-23 17:20:05 +0300

    Добавлен роут домашней страницы

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,14 @@ import type { RouteRecordRaw } from 'vue-router'
 
 export const routes: Array<RouteRecordRaw> = [
   {
+    path: '/',
+    name: 'home',
+    component: () => import('./home/HomePage.vue'),
+    meta: {
+      layout: 'app'
+    }
+  },
+  {
     path: '/test',
     name: 'test',
     component: () => import('./test/TestPage.vue'),
