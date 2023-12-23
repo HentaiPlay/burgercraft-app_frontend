2f0a0cf6d9e54d03d8156c8f259e75f2f60451bb
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-23 17:20:49 +0300

    Фикс логики с опредлением шаблона для страницы

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,23 +1,32 @@
 <script setup lang="ts">
-  import { watch, computed, ref, markRaw } from 'vue'
-  import { useRoute } from 'vue-router'
+  import { watch, ref, computed, markRaw, onMounted } from 'vue'
+  import { useRoute, useRouter } from 'vue-router'
   import { AppLayout, DefaultLayout } from '@/widgets/layouts'
 
   const layout = ref()
   const route = useRoute()
+  const router = useRouter()
+
+  // Установка шаблона
+  const setLayout = async () => {
+    switch (route.meta?.layout) {
+      case 'app':
+        layout.value = markRaw(AppLayout)
+        break
+      default:
+        layout.value = markRaw(DefaultLayout)
+    }
+  }
 
   watch(
     computed(() => route.path),
-    async () => {
-      switch (route.meta?.layout) {
-        case 'app':
-          layout.value = markRaw(AppLayout)
-          break
-        default:
-          layout.value = markRaw(DefaultLayout)
-      }
-    }
+    setLayout
   )
+
+  onMounted(async () => {
+    await router.isReady()
+    setLayout()
+  })
 </script>
 
 <template>
