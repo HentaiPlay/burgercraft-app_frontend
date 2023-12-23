65433c49fde199e6f3f33f88c0ef6591379bb89e
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-23 17:19:29 +0300

    Добавлен шаблон домашней страницы

---
+++ src/pages/home/HomePage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { useRouter } from 'vue-router'
+  const router = useRouter()
+
+  const toAuth = () => router.push({ name: 'auth' })
+  const toTest = () => router.push({ name: 'test' })
+</script>
+
+<template>
+  123
+  <div>
+    <el-button @click="toAuth">Авторизация</el-button>
+    <el-button @click="toTest">Тестовая</el-button>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
