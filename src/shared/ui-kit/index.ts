aaf4c41829fdf606c26fb1bf55a01a1ef4c9ad20
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-23 05:42:03 +0300

    Зарегистрирован компонент MyNotification, добавлен импорт иконок из Element Plus

--- src/shared/ui-kit/index.ts
+++ src/shared/ui-kit/index.ts
@@ -1,7 +1,18 @@
 import type { App } from 'vue'
+// Иконки из Element Plus
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// Кастомные компоненты
 import MyLogo from './components/MyLogo.vue'
+import MyNotification from './components/MyNotification.vue'
+
 export default {
   install: (app: App) => {
+    // Регистрация иконок из Element Plus
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(key, component)
+    }
+    // Регистрация кастомных компонентов
     app.component('MyLogo', MyLogo)
+    app.component('MyNotification', MyNotification)
   }
 }
