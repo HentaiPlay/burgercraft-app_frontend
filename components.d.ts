7ac9d55a66c825b359dded992b0f4d9381e7650b
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-24 05:06:30 +0300

    Перемещены components.d.ts и auto-imports.d.ts

--- components.d.ts
+++ components.d.ts
@@ -14,8 +14,11 @@ declare module 'vue' {
     ElForm: (typeof import('element-plus/es'))['ElForm']
     ElFormItem: (typeof import('element-plus/es'))['ElFormItem']
     ElHeader: (typeof import('element-plus/es'))['ElHeader']
+    ElIcon: (typeof import('element-plus/es'))['ElIcon']
     ElInput: (typeof import('element-plus/es'))['ElInput']
     ElMain: (typeof import('element-plus/es'))['ElMain']
+    ElOption: (typeof import('element-plus/es'))['ElOption']
+    ElSelect: (typeof import('element-plus/es'))['ElSelect']
     RouterLink: (typeof import('vue-router'))['RouterLink']
     RouterView: (typeof import('vue-router'))['RouterView']
   }
