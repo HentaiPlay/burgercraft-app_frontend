7199cd7aaedbe1b11495528d1af0e2aa5e11d995
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-24 06:34:17 +0300

    Добавлена логика авторизации и редиректа с несуществующего роута

--- src/app/providers/router.ts
+++ src/app/providers/router.ts
@@ -1,16 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from '@/pages'
 import { useUIStateStore } from '@/shared/ui-state.store'
+import { issetTokens } from '@/entities/auth/helpers/cookies.helper'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes
 })
 
-router.beforeEach((to) => {
-  // Включение прелоадеров
-  const uiStore = useUIStateStore()
-  uiStore.showPreloader()
+router.beforeEach((to, from, next) => {
   // Установка заголовков
   document.title = typeof to.meta.title === 'string' ? to.meta.title : 'BurgerCraft'
+
+  // Включение прелоадера
+  const uiStore = useUIStateStore()
+  uiStore.showPreloader()
+
+  const hasTokens = issetTokens()
+  const isAuthPath = to.name === 'auth'
+  const issetRoute = routes.some((route) => to.name === route.name)
+  const redirectPath = from?.name ? from.name : 'home'
+
+  // Сценарии валидации роутинга
+  switch (true) {
+    // Если нет токенов и роут не auth
+    case !hasTokens && !isAuthPath:
+      next({ name: 'auth' })
+      break
+    // Если не токенов, но роут auth
+    case !hasTokens && isAuthPath:
+      next()
+      break
+    // Проверка на существующий роут и редирект на предыдущий (если есть, иначе на главную страницу)
+    case !issetRoute:
+      next({ name: redirectPath })
+      break
+    default:
+      next()
+  }
 })
