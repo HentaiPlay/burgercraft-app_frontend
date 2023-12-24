d774f8bafab36cf220f3f3c2b1794d968243489b
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-24 05:14:57 +0300

    Добавлен хэндлер жизненного цикла, добавлен показ прелоадера при переходе с роутов, добавлена смена заголовков на вкладке

--- src/app/providers/router.ts
+++ src/app/providers/router.ts
@@ -1,7 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from '@/pages'
+import { useUIStateStore } from '@/shared/ui-state.store'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to) => {
+  // Включение прелоадеров
+  const uiStore = useUIStateStore()
+  uiStore.showPreloader()
+  // Установка заголовков
+  document.title = typeof to.meta.title === 'string' ? to.meta.title : 'BurgerCraft'
 })
