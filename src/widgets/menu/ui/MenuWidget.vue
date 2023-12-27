2426e8ce0e2d6cd0466a5bf33f069a5e5611bb71
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-27 03:12:31 +0300

    Добавлен компонент меню для сайдбара

---
+++ src/widgets/menu/ui/MenuWidget.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  import { useRouter, RouteRecordName } from 'vue-router'
+  const router = useRouter()
+
+  const menuItems = router.getRoutes().filter((route) => route.meta.isMenuItem)
+  const redirect = (name: RouteRecordName | undefined) => router.push({ name })
+</script>
+
+<template>
+  <!-- Меню -->
+  <el-menu class="el-menu-vertical-demo">
+    <!-- Заказы -->
+    <el-menu-item
+      v-for="(item, i) in menuItems"
+      :index="i.toString()"
+      :key="i"
+      :disabled="false"
+      @click="redirect(item.name)"
+    >
+      <el-icon>
+        <component :is="item.meta.icon" />
+      </el-icon>
+      <span>{{ item.meta.title }}</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style lang="scss" scoped></style>
