e6682f5971ffde8eb37d6e36e3b50d46302c9aa1
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-29 19:41:34 +0300

    Переименован globalComposable в global, добавлен инстанс i18n в global, добавлен метод проверки готовности всех инстансов в global композабле

--- src/shared/composables/global.ts
+++ src/shared/composables/global.ts
@@ -1,4 +1,21 @@
 import { Router } from 'vue-router'
-// Глобальный композабл для регистрации инстансов, что бы избежать
+import { Composer } from 'vue-i18n'
+import { MessageSchema, LanguageList } from '@/app/providers/i18n'
+
+interface GlobalComposable {
+  router: Router | null
+  i18n: Composer<Record<LanguageList, MessageSchema>> | null
+  isReady: () => boolean
+}
+
+const isReady = () => global.router !== null && global.i18n !== null
+
+// Глобальный композабл для регистрации желаемых инстансов, что бы избежать
 // использования provide/inject или форсировать невозможность их использования
-export default { router: null } as { router: null | Router }
+const global: GlobalComposable = {
+  router: null,
+  i18n: null,
+  isReady: isReady
+}
+
+export default global
