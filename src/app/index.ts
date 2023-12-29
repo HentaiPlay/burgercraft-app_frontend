d01b54630ea0e55d705aa99d45b8703a1cd07b82
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-29 18:19:12 +0300

    Добавлена установка i18n

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './providers'
 import { store } from './providers'
+import { i18n } from './providers'
 import '@assets/styles/index.scss'
 import uiKit from '@/shared/ui-kit'
 
-export const app = createApp(App).use(router).use(store).use(uiKit)
+export const app = createApp(App).use(router).use(store).use(i18n).use(uiKit)
