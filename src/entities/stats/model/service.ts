9b309fd242118cd78937a96493cd78e9e7bd17d5
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-30 18:01:40 +0300

    Добавлено сохранение в стор данных после запроса

--- src/entities/stats/model/service.ts
+++ src/entities/stats/model/service.ts
@@ -1,11 +1,17 @@
-import { useStatsApi } from '@/entities/stats'
-import { IStats } from './types'
 import { AxiosResponse } from 'axios'
+import { useStatsApi } from '@/entities/stats'
+import { useStatsStore } from '@/entities/stats'
 const statsApi = useStatsApi()
+const statsStore = useStatsStore()
 
 export default function useStatsService() {
   return {
-    getStats: async (): Promise<AxiosResponse<IStats[]>> => await statsApi.getStats(),
+    getStats: async () => {
+      await statsApi
+        .getStats()
+        .then((res) => statsStore.setStats(res.data))
+        .catch((e) => console.log(e))
+    },
     getStatsByUser: async (id: number): Promise<AxiosResponse<number>> => await statsApi.getStatsByUser(id)
   }
 }
