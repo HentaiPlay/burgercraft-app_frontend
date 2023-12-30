26321cd31553eee1796dca9bb1b975db7d8c1e9d
HentaiPlay <david.tonoyan16@mail.ru>
2023-12-30 03:33:59 +0300

    Удален закоментированый код

--- src/entities/user/api/user.api.ts
+++ src/entities/user/api/user.api.ts
@@ -1,9 +1,6 @@
 import apiClient from '@/app/providers/axios'
-// import { useUserStore } from '../model/store'
 
 export default function useUserApi() {
-  // const userStore = useUserStore()
-
   return {
     getUserInfo: async () => await apiClient.get('users/info'),
 
