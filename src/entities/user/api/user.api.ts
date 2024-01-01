84e446a029f5ddbfd8fb9e270a60315155b7d052
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-01 22:32:07 +0300

    Добавлен метод обновления профиля пользователя

--- src/entities/user/api/user.api.ts
+++ src/entities/user/api/user.api.ts
@@ -1,12 +1,12 @@
 import apiClient from '@/app/providers/axios'
+import { ILoginDTO } from '@/entities/auth/model/types'
 
 export default function useUserApi() {
   return {
     getUserInfo: async () => await apiClient.get('users/info'),
 
-    getAvatar: async () =>
-      await apiClient.get('users/avatar', {
-        responseType: 'blob'
-      })
+    getAvatar: async () => await apiClient.get('users/avatar', { responseType: 'blob' }),
+
+    updateProfile: async (dto: Partial<ILoginDTO>) => await apiClient.patch('users', dto)
   }
 }
