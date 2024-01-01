cf558d07a4b067f3e2491df4728fcc3554767df0
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-01 22:51:52 +0300

    Добавлена кнпока редактирования профиля и регистрации пользователя

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,17 +1,48 @@
 <script setup lang="ts">
-  import { useRouter } from 'vue-router'
-  const router = useRouter()
+  import { AuthRegister } from '@/features/auth-register'
+  import { useUserStore } from '@/entities/user'
+  import { Roles } from '@/entities/user/model/types'
+  import { EditProfile } from '@/features/edit-profile'
 
-  const toAuth = () => router.push({ name: 'auth' })
-  const toTest = () => router.push({ name: 'test' })
+  const userStore = useUserStore()
 </script>
 
 <template>
-  123
-  <div>
-    <el-button @click="toAuth">Авторизация</el-button>
-    <el-button @click="toTest">Тестовая</el-button>
+  <!-- Блок действий -->
+  <div class="actions">
+    <div class="actions__list">
+      <div class="delimiter">
+        <p>{{ $t('profile.action') }}</p>
+      </div>
+      <!-- Редактировать профиль -->
+      <div class="delimiter">
+        <EditProfile />
+      </div>
+      <!-- Регистариция нового пользователя -->
+      <AuthRegister v-if="userStore.role?.id === Roles.ADMIN" />
+    </div>
   </div>
+  <!-- Описание должности -->
+  <div>Тут описание должности</div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  .actions__list {
+    color: colors.$bg-color-overlay;
+    font-size: 0.9em;
+    @include mixins.mb(20px);
+    @include mixins.pb(20px);
+    display: flex;
+    align-items: center;
+    height: 40px;
+    border-bottom: solid 1px colors.$bg-color-overlay;
+    .delimiter {
+      @include mixins.pr(20px);
+      @include mixins.mr(20px);
+      height: 100% !important;
+      display: flex;
+      align-items: center;
+      border-right: 1px solid colors.$bg-color-overlay;
+    }
+  }
+</style>
