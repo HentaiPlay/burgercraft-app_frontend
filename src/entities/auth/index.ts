fcb179c5a03bb5fe7ff4f2c0c8bb5b8d3a100e8e
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-03 03:09:45 +0300

    Удален импорт auth композобла

--- src/entities/auth/index.ts
+++ src/entities/auth/index.ts
@@ -1,5 +1,4 @@
 import useAuthApi from './api/auth.api'
 import useAuthService from './model/service'
 import AuthForm from './ui/AuthForm.vue'
-import useAuthComposable from './composables/rules'
-export { useAuthApi, useAuthService, useAuthComposable, AuthForm }
+export { useAuthApi, useAuthService, AuthForm }
