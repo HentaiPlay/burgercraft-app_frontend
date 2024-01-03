3f72e4afa700d7b03049ceb46d07600030c24121
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-03 03:29:41 +0300

    Удалены кнопки, добавлена секция виджетов для управления профилем

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,48 +1,13 @@
 <script setup lang="ts">
-  import { AuthRegister } from '@/features/auth-register'
-  import { useUserStore } from '@/entities/user'
-  import { Roles } from '@/entities/user/model/types'
-  import { EditProfile } from '@/features/edit-profile'
-
-  const userStore = useUserStore()
+  import { ProfileActionsWidgets } from '@/widgets/profile'
 </script>
 
 <template>
   <!-- Блок действий -->
-  <div class="actions">
-    <div class="actions__list">
-      <div class="delimiter">
-        <p>{{ $t('profile.action') }}</p>
-      </div>
-      <!-- Редактировать профиль -->
-      <div class="delimiter">
-        <EditProfile />
-      </div>
-      <!-- Регистариция нового пользователя -->
-      <AuthRegister v-if="userStore.role?.id === Roles.ADMIN" />
-    </div>
-  </div>
+  <ProfileActionsWidgets />
+
   <!-- Описание должности -->
   <div>Тут описание должности</div>
 </template>
 
-<style scoped lang="scss">
-  .actions__list {
-    color: colors.$bg-color-overlay;
-    font-size: 0.9em;
-    @include mixins.mb(20px);
-    @include mixins.pb(20px);
-    display: flex;
-    align-items: center;
-    height: 40px;
-    border-bottom: solid 1px colors.$bg-color-overlay;
-    .delimiter {
-      @include mixins.pr(20px);
-      @include mixins.mr(20px);
-      height: 100% !important;
-      display: flex;
-      align-items: center;
-      border-right: 1px solid colors.$bg-color-overlay;
-    }
-  }
-</style>
+<style scoped lang="scss"></style>
