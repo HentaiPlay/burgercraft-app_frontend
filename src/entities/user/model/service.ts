515577fcb20bd2a885f3f6a089576d593f6167fc
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-03 03:21:55 +0300

    Добавлен метод получения аватара, обновления данных пользователя и удаление аккаунта

--- src/entities/user/model/service.ts
+++ src/entities/user/model/service.ts
@@ -1,8 +1,7 @@
-import { useUserApi } from '..'
-import { ILoginDTO } from '@/entities/auth/model/types'
+import { useUserApi, useUserStore } from '..'
+import { IUserUpdateDTO } from './types'
 import useMyNotification from '@/shared/ui-kit/composables/my-notification'
 import { global } from '@/shared/composables'
-import { useUserStore } from '..'
 
 export default function useUserService() {
   const userApi = useUserApi()
@@ -10,29 +9,63 @@ export default function useUserService() {
   const myNotify = useMyNotification()
 
   return {
-    updateProfile: async (dto: Partial<ILoginDTO>) => {
-      let status: boolean = false
-      await userApi
-        .updateProfile(dto)
-        .then(() => {
-          status = true
-          if (dto.name) userStore.setName(dto.name)
-          // вывод уведомления об успехе
-          myNotify({
-            type: 'success',
-            title: global.i18n?.t('notify.success') ?? 'Успех',
-            message: global.i18n?.t('editForm.status.success') ?? 'Ошибка сервера'
-          })
+    getAvatar: async (): Promise<string | void> => {
+      return await userApi
+        .getAvatar()
+        .then((res) => URL.createObjectURL(res.data))
+        .catch((err) => console.log(err))
+    },
+
+    updateProfile: async (dto: Partial<IUserUpdateDTO>): Promise<boolean> => {
+      let status: boolean = true
+      let errorMessage: string = ''
+
+      console.log(dto)
+
+      // Обновление аватарки если есть
+      if (dto.file) {
+        const avatar = dto.file
+        const formData = new FormData()
+        formData.append('file', avatar)
+        await userApi.updateAvatar(formData).catch((err) => {
+          status = false
+          errorMessage += ' ' + err.response.data.message
         })
-        .catch((err) => {
-          // вывод уведомления об ошибке
-          myNotify({
-            type: 'error',
-            title: global.i18n?.t('notify.error') ?? 'Ошибка',
-            message: err.response.data.message ?? global.i18n?.t('editForm.status.error') ?? 'Ошибка сервера'
-          })
+        delete dto.file
+      }
+
+      // Обновление никнейма и пароля если есть
+      if (dto.name || dto.password) {
+        await userApi.updateProfile(dto).catch((err) => {
+          status = false
+          errorMessage = err.response.data.message
         })
+      }
+
+      // TODO: формировать уведомления динамически в зависимости от ответа (настроить DTO на бэке)
+      if (status) {
+        // Обновление данных о пользователе
+        await userApi
+          .getUserInfo()
+          .then((res) => userStore.setUserInfo(res.data))
+          .catch((err) => console.log(err))
+
+        myNotify({
+          type: 'success',
+          title: global.i18n?.t('notify.success') ?? 'Успех',
+          message: global.i18n?.t('editForm.status.success') ?? 'Профиль успешно обновлен'
+        })
+      } else {
+        myNotify({
+          type: 'error',
+          title: global.i18n?.t('notify.error') ?? 'Ошибка',
+          message: `${global.i18n?.t('editForm.status.error')} ${errorMessage}` ?? 'Ошибка сервера'
+        })
+      }
+
       return status
-    }
+    },
+
+    removeAccount: async () => await userApi.removeAccount().catch((err) => console.log(err))
   }
 }
