bdcb9128da19e37acb6e5c19516f55c67b951f2f
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-05 21:21:54 +0300

    Импортированы композаблы из ui kit в пространство имен shared/composables

--- src/shared/composables/index.ts
+++ src/shared/composables/index.ts
@@ -1,3 +1,10 @@
+// TODO: Возможно стоит их зарегистрировать через provide/inject для DI
+
+// Импорт composable функций из ui kit
+import { useMyNotification, useMyConfirmDialog } from '../ui-kit/composables'
+
+// Обычные композаблы
 import global from './global'
 import useAudioPlayer from './audio-player'
-export { global, useAudioPlayer }
+
+export { global, useAudioPlayer, useMyNotification, useMyConfirmDialog }
