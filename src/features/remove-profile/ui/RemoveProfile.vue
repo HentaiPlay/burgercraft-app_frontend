387e0b8223c577712f0af196f91f6dd5c8ffd975
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-05 21:25:30 +0300

    Обновлен импорт композала useMyConfirmDialog

--- src/features/remove-profile/ui/RemoveProfile.vue
+++ src/features/remove-profile/ui/RemoveProfile.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
   import { Delete } from '@element-plus/icons-vue'
   import { global } from '@/shared/composables'
-  import useMyConfirmDialog from '@/shared/ui-kit/composables/my-confirm-dialog'
+  import { useMyConfirmDialog } from '@/shared/composables'
   import { useUserService } from '@/entities/user'
   import { useAuthService } from '@/entities/auth'
 
