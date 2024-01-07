a96c1411e6e8e2935f0da6a53b5c9c5940547d47
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-07 10:51:27 +0300

    Добавлен метод создания продукта и загрузки картинки, переименован метод запроса продуктов

--- src/entities/products/model/service.ts
+++ src/entities/products/model/service.ts
@@ -1,17 +1,70 @@
 import { useProductsStore } from '..'
 import { useProductsApi } from '@/entities/products'
-import { IProductDTO } from './types'
+import { ICreateProductDTO, IProductDTO } from './types'
+import { global, useMyNotification } from '@/shared/composables'
 
 export default function useUserService() {
   const productsStore = useProductsStore()
   const productsApi = useProductsApi()
+  const myNotify = useMyNotification()
 
   return {
-    getProducts: async (): Promise<IProductDTO[] | void> => {
+    setProducts: async (): Promise<IProductDTO[] | void> => {
       return await productsApi
         .getProducts()
         .then((res) => productsStore.setAllProducts(res.data))
         .catch((err) => console.log(err))
+    },
+
+    createProduct: async (dto: ICreateProductDTO): Promise<boolean> => {
+      let status: boolean = false
+      let formData: FormData | null = null
+
+      // Записываем данные нужные для изображения
+      if (dto.file) {
+        const picture = dto.file
+        formData = new FormData()
+        formData.append('data', JSON.stringify({ name: dto.name, type: dto.type }))
+        formData.append('file', picture)
+      }
+      delete dto.file
+
+      // Сохраняем данные по продукту
+      await productsApi
+        .createProduct(dto)
+        .then(() => {
+          status = true
+        })
+        .catch((err) => {
+          myNotify({
+            type: 'error',
+            title: global.i18n?.t('notify.error') ?? 'Ошибка',
+            message: err.response.data.message ?? 'Ошибка сервера'
+          })
+        })
+
+      // Сохранение картинки если есть
+      if (status && formData) {
+        await productsApi.uploadPicture(formData).catch((err) => {
+          status = false
+          myNotify({
+            type: 'error',
+            title: global.i18n?.t('notify.error') ?? 'Ошибка',
+            message: err.response.data.message ?? 'Ошибка сервера'
+          })
+        })
+      }
+
+      // Если всьо ок кидаем уведомление
+      if (status) {
+        myNotify({
+          type: 'success',
+          title: global.i18n?.t('notify.success') ?? 'Успех',
+          message: global.i18n?.t('products.form.create.success') ?? 'Продукт успешно создан'
+        })
+      }
+
+      return status
     }
   }
 }
