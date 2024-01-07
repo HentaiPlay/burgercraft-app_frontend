4b139881b832a4c9b6849af1715c4c54c37f76cd
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-07 05:12:27 +0300

    Добавлена страница Products

---
+++ src/pages/products/ProductsPage.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+  import { ref, onMounted } from 'vue'
+  import { useRoute } from 'vue-router'
+  import { useProductsService, useProductsStore } from '@/entities/products'
+  import { ProductTypeCard, ProductTypeFilter } from '@/entities/products'
+
+  const route = useRoute()
+
+  const productsService = useProductsService()
+  const productsStore = useProductsStore()
+
+  const showProductType = ref<(type: string) => boolean>((type) => {
+    const hasParam = !!route.params.type && route.params.type !== ''
+    return hasParam ? route.params.type === type : true
+  })
+
+  onMounted(async () => {
+    if (!productsStore.hasData) {
+      await productsService.getProducts()
+    }
+  })
+</script>
+
+<template>
+  <div class="products-wrapper">
+    <!-- Фильтр -->
+    <ProductTypeFilter />
+
+    <!-- Карточки типов продуктов -->
+    <template v-for="(products, type, i) in productsStore.products">
+      <ProductTypeCard
+        v-if="showProductType(type)"
+        :products="products"
+        :type="type"
+        :key="i"
+      />
+    </template>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
