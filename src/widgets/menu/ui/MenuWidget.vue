713ac4ed8eb2bc3427bd62c736b0557fb1ba61ee
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-07 05:01:51 +0300

    Изменена логика парсинга роута, так как поменялась meta, изменена логика опредления дефолтного активного роута, добавлен фильтр очередности роутов в меню

--- src/widgets/menu/ui/MenuWidget.vue
+++ src/widgets/menu/ui/MenuWidget.vue
@@ -1,27 +1,39 @@
 <script setup lang="ts">
+  import { computed } from 'vue'
   import { useRoute, useRouter } from 'vue-router'
+
   const route = useRoute()
   const router = useRouter()
-  const menuItems = router.getRoutes().filter((route) => route.meta.isMenuItem)
+
+  const menuItems = router
+    .getRoutes()
+    .filter((route) => (route.meta.isMenuItem && route.meta.menuOptions ? route : null))
+    .sort((a, b) => {
+      const first = a?.meta.menuOptions?.orderNumber
+      const second = b?.meta.menuOptions?.orderNumber
+      return first && second ? first - second : 0
+    })
+
+  const activeMenu = computed(() => route.matched[0]?.path)
 </script>
 
 <template>
   <!-- Меню -->
   <el-menu
     class="el-menu-vertical-demo"
-    :default-active="route.path"
+    :default-active="activeMenu"
     router
   >
     <!-- Заказы -->
     <el-menu-item
-      v-for="(item, i) in menuItems"
+      v-for="item in menuItems"
       :index="item.path"
-      :key="i"
+      :key="item.name"
       :route="{ name: item.name }"
       :disabled="!item.meta.hasPermission"
     >
       <el-icon>
-        <component :is="item.meta.icon" />
+        <component :is="item.meta.menuOptions?.icon" />
       </el-icon>
       <span>{{ $t(item.meta.title) }}</span>
     </el-menu-item>
