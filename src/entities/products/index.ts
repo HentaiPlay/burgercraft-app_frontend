629a1734d130b45ac81299960c2fe08f4b8e3aad
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-07 05:11:25 +0300

    Импортированы api, store, service и ui компоненты сущности products в пространство имен сущности

---
+++ src/entities/products/index.ts
@@ -0,0 +1,7 @@
+import ProductCard from './ui/ProductCard.vue'
+import ProductTypeCard from './ui/ProductTypeCard.vue'
+import ProductTypeFilter from './ui/ProductTypeFilter.vue'
+import useProductsApi from './api/products.api'
+import { useProductsStore } from './model/store'
+import useProductsService from './model/service'
+export { ProductCard, ProductTypeCard, ProductTypeFilter, useProductsStore, useProductsApi, useProductsService }
