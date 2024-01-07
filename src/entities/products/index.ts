661b683819204c5d98d6563be55ecc56a61313a3
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-07 10:53:51 +0300

    Импортирован компонент формы в пространство имен сущности products

--- src/entities/products/index.ts
+++ src/entities/products/index.ts
@@ -1,7 +1,16 @@
 import ProductCard from './ui/ProductCard.vue'
 import ProductTypeCard from './ui/ProductTypeCard.vue'
 import ProductTypeFilter from './ui/ProductTypeFilter.vue'
+import ProductForm from './ui/ProductForm.vue'
 import useProductsApi from './api/products.api'
 import { useProductsStore } from './model/store'
 import useProductsService from './model/service'
-export { ProductCard, ProductTypeCard, ProductTypeFilter, useProductsStore, useProductsApi, useProductsService }
+export {
+  ProductCard,
+  ProductTypeCard,
+  ProductTypeFilter,
+  ProductForm,
+  useProductsStore,
+  useProductsApi,
+  useProductsService
+}
