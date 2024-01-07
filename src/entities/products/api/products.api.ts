323bbf27ffed14990d390478cc18371f7910c47e
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-07 10:47:58 +0300

    Добавлен запрос создания продукта, обновления и загрузки картинки

--- src/entities/products/api/products.api.ts
+++ src/entities/products/api/products.api.ts
@@ -1,7 +1,17 @@
 import apiClient from '@/app/providers/axios'
+import { ICreateProductDTO } from '../model/types'
 
 export default function useProductsApi() {
   return {
-    getProducts: async () => await apiClient.get('products')
+    getProducts: async () => await apiClient.get('products'),
+
+    createProduct: async (dto: ICreateProductDTO) => await apiClient.post('products', dto),
+
+    updateProduct: async (dto: Omit<ICreateProductDTO, 'type'>) => await apiClient.patch('prosucts', dto),
+
+    uploadPicture: async (dto: FormData) =>
+      await apiClient.put('products/photo', dto, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      })
   }
 }
