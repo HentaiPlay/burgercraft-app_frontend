63d5a59b24a0f726e107acb769ef160f43ab40ce
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-09 07:47:43 +0300

    Добавлена страница orders

---
+++ src/pages/orders/OrdersPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { onMounted } from 'vue'
+  import { OrdersTable } from '@/entities/orders'
+  import { useOrdersStore, useOrdersService } from '@/entities/orders'
+
+  const ordersStore = useOrdersStore()
+  const ordersService = useOrdersService()
+
+  onMounted(async () => {
+    if (!ordersStore.hasData) {
+      await ordersService.setOrders()
+    }
+  })
+</script>
+
+<template>
+  <OrdersTable :orders="ordersStore.orders" />
+</template>
+
+<style lang="scss" scoped></style>
