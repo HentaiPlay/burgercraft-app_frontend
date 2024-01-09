4dcfdd2385e67bc94024b1399d876186e2bd7942
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-09 07:46:49 +0300

    Импортированы api, store, service и ui сущности orders в пространстово имен сущности orders

---
+++ src/entities/orders/index.ts
@@ -0,0 +1,5 @@
+import OrdersTable from './ui/OrdersTable.vue'
+import useOrdersApi from './api/orders.api'
+import { useOrdersStore } from './model/store'
+import useOrdersService from './model/service'
+export { OrdersTable, useOrdersApi, useOrdersStore, useOrdersService }
