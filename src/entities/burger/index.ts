c2c1c4858eb8528ee0b55fa3207e82ec5509b4b7
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-13 23:25:05 +0300

    Импортированы api service и ui сущности в burger в пространство имен сущности

---
+++ src/entities/burger/index.ts
@@ -0,0 +1,4 @@
+import BurgerConstructor from './ui/BurgerConstructor.vue'
+import useBurgerApi from './api/burger.api'
+import useBurgerService from './model/service'
+export { BurgerConstructor, useBurgerApi, useBurgerService }
