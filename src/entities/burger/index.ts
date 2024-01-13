862daf7ffb6a2b182038ff27903bc3d6d25e45e5
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-14 02:51:19 +0300

    Добавлены store и компонент формы в пространство имен сущности burger

--- src/entities/burger/index.ts
+++ src/entities/burger/index.ts
@@ -1,4 +1,6 @@
 import BurgerConstructor from './ui/BurgerConstructor.vue'
+import BurgerForm from './ui/BurgerForm.vue'
 import useBurgerApi from './api/burger.api'
 import useBurgerService from './model/service'
-export { BurgerConstructor, useBurgerApi, useBurgerService }
+import { useBurgerStore } from './model/store'
+export { BurgerConstructor, BurgerForm, useBurgerApi, useBurgerService, useBurgerStore }
