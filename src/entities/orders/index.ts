6aa229a036ff62432e1a1c5dd684e82207f5e4c4
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-15 01:20:31 +0300

    Импортирован компонент OrderForm в пространство имен сущности orders

--- src/entities/orders/index.ts
+++ src/entities/orders/index.ts
@@ -1,5 +1,6 @@
 import OrdersTable from './ui/OrdersTable.vue'
+import OrderForm from './ui/OrderForm.vue'
 import useOrdersApi from './api/orders.api'
 import { useOrdersStore } from './model/store'
 import useOrdersService from './model/service'
-export { OrdersTable, useOrdersApi, useOrdersStore, useOrdersService }
+export { OrdersTable, OrderForm, useOrdersApi, useOrdersStore, useOrdersService }
