d8ec0817b4977d87f044bf7283d4f26f01a32111
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-15 01:21:28 +0300

    Добавлена таблица заказов и панель с информацией и кнпокой создания заказа

--- src/pages/orders/OrdersPage.vue
+++ src/pages/orders/OrdersPage.vue
@@ -1,20 +1,54 @@
 <script setup lang="ts">
   import { onMounted } from 'vue'
-  import { OrdersTable } from '@/entities/orders'
-  import { useOrdersStore, useOrdersService } from '@/entities/orders'
+  import { OrdersTable, OrderForm, useOrdersStore, useOrdersService } from '@/entities/orders'
 
   const ordersStore = useOrdersStore()
   const ordersService = useOrdersService()
 
   onMounted(async () => {
-    if (!ordersStore.hasData) {
-      await ordersService.setOrders()
+    if (!ordersStore.hasOrderListData) {
+      await ordersService.setOrderList()
     }
   })
 </script>
 
 <template>
-  <OrdersTable :orders="ordersStore.orders" />
+  <div class="panel">
+    <!-- Предупреждение -->
+    <div class="panel__warning">
+      <el-icon><WarnTriangleFilled /></el-icon>
+      <span>{{ $t('orders.info') }}</span>
+    </div>
+    <OrderForm mode="create" />
+  </div>
+
+  <!-- Таблица заказов -->
+  <OrdersTable />
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .panel {
+    @include mixins.mb(20px);
+    @include mixins.pb(20px);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 40px;
+    border-bottom: solid 1px colors.$bg-color-overlay;
+  }
+  .panel__warning {
+    display: flex;
+    align-items: center;
+    i {
+      font-size: 1.4em;
+      @include mixins.mr(10px);
+    }
+    span,
+    i {
+      color: colors.$warning !important;
+    }
+  }
+  .order-list {
+    display: flex;
+  }
+</style>
