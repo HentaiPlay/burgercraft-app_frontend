613fa880f873c38a745d276f7d6a235eaac915b7
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-16 13:47:33 +0300

    Обновлены типы IBurgerDTO и добавлен тип для активного бургера

--- src/entities/burger/model/store.ts
+++ src/entities/burger/model/store.ts
@@ -1,19 +1,18 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 import { cloneDeep } from 'lodash'
-import { IBurger, IBurgerIngredientOptions } from './types'
+import { IBurgerDTO, IActiveBurgerDTO, IBurgerIngredientOptions } from './types'
 
 export const useBurgerStore = defineStore('burger', () => {
-  const templateBurger: IBurger = {
-    orderId: 0,
+  const templateBurger: IActiveBurgerDTO = {
     price: 0,
     ingredients: []
   }
-  const burger = ref<IBurger>(cloneDeep(templateBurger))
+  const burger = ref<IActiveBurgerDTO>(cloneDeep(templateBurger))
 
   const ingredients = computed(() => burger.value.ingredients)
 
-  function setBurger(dto: IBurger) {
+  function setBurger(dto: IBurgerDTO) {
     burger.value = dto
   }
 
