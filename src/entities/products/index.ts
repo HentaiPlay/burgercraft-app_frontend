0aaca491e79b4f13c33c780452797495522bfc6b
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-16 14:51:46 +0300

    Импортирован компонент OrdersProductsList в пространстов имен сущности products

--- src/entities/products/index.ts
+++ src/entities/products/index.ts
@@ -3,6 +3,7 @@ import ProductTypeCard from './ui/ProductTypeCard.vue'
 import ProductTypeFilter from './ui/ProductTypeFilter.vue'
 import ProductForm from './ui/ProductForm.vue'
 import IngredientList from './ui/IngredientList.vue'
+import OrdersProductsList from './ui/OrdersProductsList.vue'
 import useProductsApi from './api/products.api'
 import { useProductsStore } from './model/store'
 import useProductsService from './model/service'
@@ -12,6 +13,7 @@ export {
   ProductTypeFilter,
   ProductForm,
   IngredientList,
+  OrdersProductsList,
   useProductsStore,
   useProductsApi,
   useProductsService
