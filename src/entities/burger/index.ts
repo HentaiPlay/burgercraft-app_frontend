68da5ed631cc70e02535884a16b8fc4ca89f128c
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-19 04:32:15 +0300

    Импортиррован компонент BurgerList в пространство имен сущности burger

--- src/entities/burger/index.ts
+++ src/entities/burger/index.ts
@@ -1,6 +1,7 @@
 import BurgerConstructor from './ui/BurgerConstructor.vue'
 import BurgerForm from './ui/BurgerForm.vue'
+import BurgerList from './ui/BurgerList.vue'
 import useBurgerApi from './api/burger.api'
 import useBurgerService from './model/service'
 import { useBurgerStore } from './model/store'
-export { BurgerConstructor, BurgerForm, useBurgerApi, useBurgerService, useBurgerStore }
+export { BurgerConstructor, BurgerForm, BurgerList, useBurgerApi, useBurgerService, useBurgerStore }
