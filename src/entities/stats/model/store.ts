ce816bfddd5249cb9de2274d3fc4cf017c446b00
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-22 01:44:48 +0300

    Добавлен стейт, геттер и экшен для суммы заработанной за деньв модуле stats

--- src/entities/stats/model/store.ts
+++ src/entities/stats/model/store.ts
@@ -3,13 +3,18 @@ import { computed, ref } from 'vue'
 import { IStats } from './types'
 
 export const useStatsStore = defineStore('stats', () => {
+  const sum = ref<number | null>(null)
   const stats = ref<IStats[]>([])
 
+  const hasSum = computed(() => sum.value !== null)
   const hasStats = computed(() => Boolean(stats.value.length))
 
+  function setSum(dto: number) {
+    sum.value = dto
+  }
   function setStats(dto: IStats[]) {
     stats.value = dto
   }
 
-  return { stats, hasStats, setStats }
+  return { stats, sum, hasSum, hasStats, setSum, setStats }
 })
