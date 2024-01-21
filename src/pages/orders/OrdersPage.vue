6ee487ea4f4b5b8a2153651554def857f5aecf90
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-22 01:48:42 +0300

    Добавлена панель с отображением суммы заработанной за день

--- src/pages/orders/OrdersPage.vue
+++ src/pages/orders/OrdersPage.vue
@@ -2,14 +2,20 @@
   import { onMounted } from 'vue'
   import { useUserStore } from '@/entities/user'
   import { OrdersTable, OrderForm, useOrdersStore, useOrdersService } from '@/entities/orders'
+  import { useStatsStore, useStatsService } from '@/entities/stats'
 
   const ordersStore = useOrdersStore()
   const ordersService = useOrdersService()
   const userStore = useUserStore()
+  const statsStore = useStatsStore()
+  const statsService = useStatsService()
 
   onMounted(async () => {
     if (!ordersStore.hasOrderListData) {
       await ordersService.setOrderList()
+    }
+    if (!statsStore.hasSum) {
+      await statsService.getStatsByUser()
     }
   })
 </script>
@@ -25,6 +31,11 @@
       v-if="userStore.role?.accessList.interfaces.orders.create"
       mode="create"
     />
+  </div>
+
+  <div class="panel__sum">
+    <span>{{ $t('orders.counterSum', { number: statsStore.sum }) }}</span>
+    <el-icon><Coin /></el-icon>
   </div>
 
   <!-- Таблица заказов -->
@@ -54,7 +65,18 @@
       line-height: 1.2rem;
     }
   }
-  .order-list {
+  .panel__sum {
     display: flex;
+    align-items: center;
+    @include mixins.mb(10px);
+    @include mixins.pb(20px);
+    color: colors.$success;
+    border-bottom: 1px solid colors.$bg-color-overlay;
+    span {
+      font-size: 0.9em;
+    }
+    i {
+      @include mixins.ml(10px);
+    }
   }
 </style>
