6e26f7111c83927a962df202d0f557fd29bc56bc
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-21 03:19:17 +0300

    Добавлен экшен перерасчета стоимости заказа, при обновлении списка бургеров или продуктов заказа запускается перерасчет заказа, добавлен стейт для сохранения первоначального состояния заказа при редактировании, обновлен темплейт для стейта заказа

--- src/entities/orders/model/store.ts
+++ src/entities/orders/model/store.ts
@@ -1,16 +1,18 @@
 import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 import { cloneDeep } from 'lodash'
-import { IActiveOrderDTO, IOrderDTO, IOrderListElement, IOrderProduct } from './types'
+import { IActiveOrderDTO, IOrderListElement, IOrderProduct } from './types'
 import { IBurgerDTO } from '@/entities/burger/model/types'
 
 export const useOrdersStore = defineStore('orders', () => {
   const temaplteActiveOrder: IActiveOrderDTO = {
+    price: 0,
     burgers: [],
     ordersProducts: []
   }
   const orderList = ref<IOrderListElement[]>([])
   const activeOrder = ref<IActiveOrderDTO>(cloneDeep(temaplteActiveOrder))
+  const oldOrder = ref<IActiveOrderDTO>(cloneDeep(temaplteActiveOrder))
 
   const hasOrderListData = computed(() => !!orderList.value.length)
   const hasActiveOrderData = computed(() => activeOrder.value.burgers.length || activeOrder.value.ordersProducts.length)
@@ -19,29 +21,48 @@ export const useOrdersStore = defineStore('orders', () => {
     orderList.value = dto
   }
 
-  function setActiveOrder(dto: IOrderDTO) {
+  function setActiveOrder(dto: IActiveOrderDTO) {
     activeOrder.value = dto
+    oldOrder.value = cloneDeep(dto)
   }
 
-  // Бургер
+  // Бургеры
   function addBurger(dto: IBurgerDTO) {
     activeOrder.value.burgers.push(dto)
+    countPrice()
   }
   function editBurger(dto: IBurgerDTO, index: number) {
     activeOrder.value.burgers[index] = dto
+    countPrice()
   }
   function removeBurger(index: number) {
     delete activeOrder.value.burgers[index]
     activeOrder.value.burgers = activeOrder.value.burgers.filter(Boolean)
+    countPrice()
   }
 
   // Продукты к заказу
   function addOrdersProduct(dto: IOrderProduct) {
     activeOrder.value?.ordersProducts.push(dto)
+    countPrice()
   }
   function removeOrdersProduct(index: number) {
     delete activeOrder.value.ordersProducts[index]
     activeOrder.value.ordersProducts = activeOrder.value.ordersProducts.filter(Boolean)
+    countPrice()
+  }
+
+  // Пересчет стоимости
+  function countPrice() {
+    activeOrder.value.price = 0
+    activeOrder.value.price += activeOrder.value.burgers.reduce((totalPrice, burger) => {
+      totalPrice += burger.price
+      return totalPrice
+    }, 0)
+    activeOrder.value.price += activeOrder.value.ordersProducts.reduce((totalPrice, product) => {
+      totalPrice += product.price
+      return totalPrice
+    }, 0)
   }
 
   function clearActiveOrder() {
@@ -56,6 +77,7 @@ export const useOrdersStore = defineStore('orders', () => {
   return {
     orderList,
     activeOrder,
+    oldOrder,
     hasOrderListData,
     hasActiveOrderData,
     setOrderList,
