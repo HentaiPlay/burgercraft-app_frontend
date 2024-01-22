3ee1bb615c600d37491ff9e4abeb32463ac5b5cf
HentaiPlay <david.tonoyan16@mail.ru>
2024-01-22 03:06:56 +0300

    Обновлена карта проекта

--- components.d.ts
+++ components.d.ts
@@ -10,6 +10,7 @@ declare module 'vue' {
     ElAside: (typeof import('element-plus/es'))['ElAside']
     ElButton: (typeof import('element-plus/es'))['ElButton']
     ElContainer: (typeof import('element-plus/es'))['ElContainer']
+    ElDialog: (typeof import('element-plus/es'))['ElDialog']
     ElFooter: (typeof import('element-plus/es'))['ElFooter']
     ElForm: (typeof import('element-plus/es'))['ElForm']
     ElFormItem: (typeof import('element-plus/es'))['ElFormItem']
@@ -17,9 +18,22 @@ declare module 'vue' {
     ElIcon: (typeof import('element-plus/es'))['ElIcon']
     ElInput: (typeof import('element-plus/es'))['ElInput']
     ElMain: (typeof import('element-plus/es'))['ElMain']
+    ElMenu: (typeof import('element-plus/es'))['ElMenu']
+    ElMenuItem: (typeof import('element-plus/es'))['ElMenuItem']
     ElOption: (typeof import('element-plus/es'))['ElOption']
+    ElPopover: (typeof import('element-plus/es'))['ElPopover']
     ElSelect: (typeof import('element-plus/es'))['ElSelect']
+    ElSwitch: (typeof import('element-plus/es'))['ElSwitch']
+    ElTable: (typeof import('element-plus/es'))['ElTable']
+    ElTableColumn: (typeof import('element-plus/es'))['ElTableColumn']
+    ElTag: (typeof import('element-plus/es'))['ElTag']
+    ElText: (typeof import('element-plus/es'))['ElText']
+    ElTooltip: (typeof import('element-plus/es'))['ElTooltip']
     RouterLink: (typeof import('vue-router'))['RouterLink']
     RouterView: (typeof import('vue-router'))['RouterView']
+  }
+  export interface ComponentCustomProperties {
+    vLoading: (typeof import('element-plus/es'))['ElLoadingDirective']
+    vPopover: (typeof import('element-plus/es'))['ElPopoverDirective']
   }
 }
